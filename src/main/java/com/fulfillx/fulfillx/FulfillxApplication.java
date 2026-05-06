2fde712e6d301ff493d291c70c8bf3d37d68c56c
vibehav <vibehav@protonmail.com>
2026-05-06 19:24:41 +0530

    Project setup, parent pom, service modules, docker-compose

---
+++ src/main/java/com/fulfillx/fulfillx/FulfillxApplication.java
@@ -0,0 +1,13 @@
+package com.fulfillx.fulfillx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillxApplication.class, args);
+	}
+
+}
