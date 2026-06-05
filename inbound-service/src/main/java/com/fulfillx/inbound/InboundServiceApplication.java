8994bd563cf507e64849fe5108d84e4bc33fcfa8
vibehav <vibehav@protonmail.com>
2026-06-05 23:52:43 +0530

    rabbitmq bug fix

--- inbound-service/src/main/java/com/fulfillx/inbound/InboundServiceApplication.java
+++ inbound-service/src/main/java/com/fulfillx/inbound/InboundServiceApplication.java
@@ -3,8 +3,10 @@ package com.fulfillx.inbound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 @ComponentScan(basePackages =
         {"com.fulfillx.inbound",
         "com.fulfillx.common"
@@ -17,5 +19,7 @@ public class InboundServiceApplication {
                 ctx.containsBean("jwtAuthFilter"));
         System.out.println(">>> JwtUtil bean exists: " +
                 ctx.containsBean("jwtUtil"));
+        System.out.println(">>> RabbitAdmin bean exists: " + ctx.containsBean("rabbitAdmin"));
+        System.out.println(">>> RabbitTemplate bean exists: " + ctx.containsBean("rabbitTemplate"));
     }
 }
--- inbound-service/src/main/java/com/fulfillx/inbound/config/RabbitMQConfig.java
+++ inbound-service/src/main/java/com/fulfillx/inbound/config/RabbitMQConfig.java
@@ -3,6 +3,7 @@ package com.fulfillx.inbound.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +19,7 @@ public class RabbitMQConfig {
 
     public static final String STOCK_DLQ = "stock.received.dlq";
     public static final String DLX_EXCHANGE = "inbound.dlx.exchange";
-    public static final String STOCK_DLQ_ROUTING_KEY = "stock.received.dlq.routing.key";
+    public static final String STOCK_DLQ_ROUTING_KEY = "stock.received.dlq.key";
 
 
     // until a worker service comes and picks it(conusmer) // QUEUE
@@ -52,20 +53,19 @@ public class RabbitMQConfig {
     }
 
     // Declare the Dead Letter Queue (also a Quorum!) and Exchange
-
     @Bean
-    public Queue deadLetterQueue() {
+    public Queue StockReceivedDLQ() {
         return QueueBuilder.durable(STOCK_DLQ).quorum().build();
     }
 
     @Bean
-    public DirectExchange deadLetterExchange() {
+    public DirectExchange StockReceivedDLX() {
         return new DirectExchange(DLX_EXCHANGE);
     }
     @Bean
     public Binding deadLetterBinding() {
-        return BindingBuilder.bind(deadLetterQueue())
-                .to(deadLetterExchange())
+        return BindingBuilder.bind(StockReceivedDLQ())
+                .to(StockReceivedDLX())
                 .with(STOCK_DLQ_ROUTING_KEY);
     }
 
@@ -96,8 +96,8 @@ public class RabbitMQConfig {
             // future work: save to outbox table in db for cron job
         });
 
+            //Returns Callback was true
         template.setReturnsCallback(returned -> {
-            // you have publisher-returns: true but no ReturnsCallback — add this too
             log.error("Message returned: exchange={}, routingKey={}, replyText={}",
                     returned.getExchange(),
                     returned.getRoutingKey(),
@@ -105,6 +105,13 @@ public class RabbitMQConfig {
         });
 
         return template;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.setAutoStartup(true);
+        return admin;
     }
 
 
