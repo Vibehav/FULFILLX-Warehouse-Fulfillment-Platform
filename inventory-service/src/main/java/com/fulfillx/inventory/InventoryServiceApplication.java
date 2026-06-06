55d748490c940ab38085b3d2d66debde39ac392a
vibehav <vibehav@protonmail.com>
2026-06-06 11:45:08 +0530

    Changes in inventory service application

--- inventory-service/src/main/java/com/fulfillx/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/fulfillx/inventory/InventoryServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableCaching
+@EnableRetry
 @ComponentScan(basePackages = {
         "com.fulfillx.inventory",
         "com.fulfillx.common"
